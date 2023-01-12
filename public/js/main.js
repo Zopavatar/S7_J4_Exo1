3c6379a28d20679f34ecae9fe4ff26236fc379c4
unknown <Charlotte Landuyt>
2023-01-12 14:23:42 +0100

    exo1

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+let titre = document.getElementsByTagName('h1');
+
+console.log(titre);
+
+
+let paragraphes = document.getElementsByTagName("p");
+
+
+let sousTitre = document.getElementsByTagName("h3");
+
+for(i = 0; i < sousTitre.length; i++){
+    let h3 = sousTitre[i];
+    console.log(h3);
+}
+
+
+let paragraphe2 = paragraphes[1];
+
+console.log(paragraphe2);
+
+
+let lesLi = document.getElementsByTagName("li");
+let li3 = lesLi[2];
+
+console.log(li3);
+
+
